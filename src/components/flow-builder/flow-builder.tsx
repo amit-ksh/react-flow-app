1a032e4c241fe41e52828936b6ea80dd149cd337
Amit Sharma <ksamit1110@gmail.com>
2025-10-05 02:10:15 +0530

    feat: update node and edge on connection with validation

--- src/components/flow-builder/flow-builder.tsx
+++ src/components/flow-builder/flow-builder.tsx
@@ -1,64 +1,119 @@
-import { useState, useCallback, useRef, type DragEvent } from 'react';
-import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge, type NodeChange, type EdgeChange, type Connection, useReactFlow } from '@xyflow/react';
-import '@xyflow/react/dist/style.css';
-import { NODE_COMPONENTS } from './node';
-import { useDnD } from '@/contexts/dnd-context';
-import type { EdgeData, FlowBuilderEdge, FlowBuilderNode, TextNodeData } from '@/types/flow-builder';
-import { NodeType } from '@/lib/flow-builder';
-import { useLocalStorageQuery } from '@/hooks/use-local-storage';
-import { QUERY_KEYS } from '@/lib/query-keys';
- 
+import { useState, useCallback, useRef, type DragEvent } from "react";
+import {
+  ReactFlow,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+  type NodeChange,
+  type EdgeChange,
+  type Connection,
+  useReactFlow,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+import { NODE_COMPONENTS } from "./node";
+import { useDnD } from "@/contexts/dnd-context";
+import type {
+  EdgeData,
+  FlowBuilderEdge,
+  FlowBuilderNode,
+  TextNodeData,
+} from "@/types/flow-builder";
+import { NodeRules, NodeType, type EdgeLike } from "@/lib/flow-builder";
+import { useLocalStorageQuery } from "@/hooks/use-local-storage";
+import { QUERY_KEYS } from "@/lib/query-keys";
+
 const initialNodes = [
-  { id: 'n1', text: 'Node 1', type: 'whatsapp', to: ['n2'] },
-  { id: 'n2', text: 'Node 2', type: 'whatsapp', to: [] },
+  { id: "n1", text: "Node 1", type: "whatsapp", to: ["n2"] },
+  { id: "n2", text: "Node 2", type: "whatsapp", to: [] },
 ];
 
-
-export  function FlowBuilder() {
+export function FlowBuilder() {
   const reactFlow = useReactFlow();
-  const { data: nodeData } = useLocalStorageQuery<TextNodeData[]>(QUERY_KEYS.NODES, initialNodes);
-
-  const initialReactFlowNodes: FlowBuilderNode<TextNodeData>[] = nodeData?.map((data) => ({
-        id: data.id,
-        type: NodeType.Text,
-        position: { x: Math.random() * 400, y: Math.random() * 400 },
-        data,
-      })) ?? [];
-  const initialEdges: FlowBuilderEdge<EdgeData>[] = initialReactFlowNodes.flatMap((node) =>
-    node.data.to.map((targetId) => ({
-      id: `e${node.id}-${targetId}`, source: node.id, target: targetId, data: { from: node.id, to: targetId },
-    }))
+  const { data: nodeData } = useLocalStorageQuery<TextNodeData[]>(
+    QUERY_KEYS.NODES,
+    initialNodes
   );
 
-  const [nodes, setNodes] = useState<FlowBuilderNode<TextNodeData>[]>(initialReactFlowNodes);
+  const initialReactFlowNodes: FlowBuilderNode<TextNodeData>[] =
+    nodeData?.map((data) => ({
+      id: data.id,
+      type: NodeType.Text,
+      position: { x: Math.random() * 400, y: Math.random() * 400 },
+      data,
+    })) ?? [];
+  const initialEdges: FlowBuilderEdge<EdgeData>[] =
+    initialReactFlowNodes.flatMap((node) =>
+      node.data.to.map((targetId) => ({
+        id: `e${node.id}-${targetId}`,
+        source: node.id,
+        target: targetId,
+        data: { from: node.id, to: targetId },
+      }))
+    );
+
+  const [nodes, setNodes] = useState<FlowBuilderNode<TextNodeData>[]>(
+    initialReactFlowNodes
+  );
   const [edges, setEdges] = useState<FlowBuilderEdge<EdgeData>[]>(initialEdges);
 
   const onNodesChange = useCallback(
-    (changes: NodeChange<typeof nodes[number]>[]) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
-    [],
+    (changes: NodeChange<(typeof nodes)[number]>[]) =>
+      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    []
   );
   const onEdgesChange = useCallback(
-    (changes: EdgeChange<typeof edges[number]>[]) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
-    [],
+    (changes: EdgeChange<(typeof edges)[number]>[]) =>
+      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    []
   );
   const onConnect = useCallback(
     (params: Connection) => {
-      setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot))
+      const isInvalid = [
+        NodeRules.validateSelfLoops([...edges, params] as EdgeLike[]),
+        NodeRules.validateDuplicateEdges([...edges, params] as EdgeLike[]),
+        NodeRules.validateMaxOutgoing([...edges, params] as EdgeLike[], 1),
+      ].some((v) => v);
+
+      if (isInvalid) {
+        console.log("Invalid connection", params);
+        return;
+      }
+
+      if (!params.source || !params.target) {
+        return;
+      }
+
+      const sourceNodeId = params.source;
+      const targetNodeId = params.target;
+      console.log("Connecting", sourceNodeId, "to", targetNodeId);
+      setNodes((nodesSnapshot) =>
+        nodesSnapshot.map((node) => {
+          if (node.id === sourceNodeId && targetNodeId) {
+            const to = Array.from(
+              new Set([...(node.data.to || []), targetNodeId])
+            );
+            return { ...node, data: { ...node.data, to } };
+          }
+          return node;
+        })
+      );
+
+      setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot));
     },
-    [],
+    [edges]
   );
-   const reactFlowWrapper = useRef(null);
+  const reactFlowWrapper = useRef(null);
   const { type } = useDnD();
 
   const onDragOver = useCallback((event: DragEvent) => {
     event.preventDefault();
-    event.dataTransfer.dropEffect = 'move';
+    event.dataTransfer.dropEffect = "move";
   }, []);
- 
+
   const onDrop = useCallback(
     (event: DragEvent) => {
       event.preventDefault();
- 
+
       if (!type) {
         return;
       }
@@ -67,20 +122,19 @@ export  function FlowBuilder() {
         x: event.clientX,
         y: event.clientY,
       });
-      const id = crypto.randomUUID()
+      const id = crypto.randomUUID();
       const newNode: FlowBuilderNode<TextNodeData> = {
         id,
         type,
         position,
-        data: { id, text: `Click to edit text`, type: 'whatsapp', to: [] },
-      } ;
- 
+        data: { id, text: `Click to edit text`, type: "whatsapp", to: [] },
+      };
+
       setNodes((nds) => [...nds, newNode]);
     },
-    [reactFlow, type],
+    [reactFlow, type]
   );
 
- 
   return (
     <div ref={reactFlowWrapper} className="size-full">
       <ReactFlow
@@ -90,9 +144,9 @@ export  function FlowBuilder() {
         onEdgesChange={onEdgesChange}
         nodeTypes={NODE_COMPONENTS}
         onConnect={onConnect}
-        fitView
         onDragOver={onDragOver}
         onDrop={onDrop}
+        fitView
       />
     </div>
   );
--- src/lib/flow-builder.ts
+++ src/lib/flow-builder.ts
@@ -1,5 +1,55 @@
+import type { Edge, Node } from "@xyflow/react";
+
 export const NodeType = {
     Text: "textNode",
 } as const
 
 export type NodeType = typeof NodeType[keyof typeof NodeType];
+
+
+export type EdgeLike = Pick<Edge, 'id' | 'source' | 'target'>
+export type NodeLike = Pick<Node, 'id'>
+
+
+export const NodeRules = {
+    validateSelfLoops(edges: EdgeLike[]): boolean {
+        const byNode = new Map<string, number>()
+        for (const e of edges || []) {
+            if (e.source === e.target) {
+                let count = byNode.get(e.source) ?? 0
+                count += 1
+                byNode.set(e.source, count)
+            }
+        }
+
+        const hasSelfLoop = Array.from(byNode.values()).some(count => count > 0)
+        return hasSelfLoop
+    },
+
+    validateDuplicateEdges(edges: EdgeLike[]): boolean {
+        const pairKey = (s: string, t: string) => `${s}=>${t}`
+        const byPair = new Map<string, number>()
+        for (const e of edges || []) {
+            const k = pairKey(e.source, e.target)
+            let count = byPair.get(k) ?? 0
+            count += 1
+            byPair.set(k, count)
+        }
+
+        const hasDuplicate = Array.from(byPair.values()).some(count => count > 1)
+        return hasDuplicate
+    },
+
+    validateMaxOutgoing(edges: EdgeLike[], max = 1): boolean {
+        console.log(edges)
+        const bySource = new Map<string, number>()
+        for (const e of edges || []) {
+            let count = bySource.get(e.source) || 0
+            count += 1
+            bySource.set(e.source, count)
+        }
+
+        const hasMaxOutgoing = Array.from(bySource.values()).some(occurrences => occurrences > max)
+        return hasMaxOutgoing
+    },
+}
